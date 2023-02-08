0c4ae14605f0ad0191c7cc72f08cc1c49e5bc62f
Matse2005 <matse.vanhorebeek@gmail.com>
2023-02-08 12:56:20 +0100

    Updated the footer: TailwindCSS 2 -> TailwindCSS 3

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
-<div class="footer text-sm bg-petrol text-white flex flex-wrap pt-7 pr-5 pb-5 pl-40 relative xs:pl-7">
-    <div class="menu w-1/6 pb-5 pl-10 relative md:w-1/4 sm:w-1/2 xxs:w-full">
-        <h3 class="font-bold text-white text-base">ons centrum</h3>
+<div class="relative flex flex-wrap pb-5 pr-5 text-sm text-white md:pl-40 footer bg-petrol pt-7 pl-7">
+    <div class="relative w-full pb-5 pl-10 lg:w-1/6 menu md:w-1/4 sm:w-1/2">
+        <h3 class="text-base font-bold text-white">ons centrum</h3>
         <ul>
             <li><a href="/centrumreglement" target="_blank">centrumreglement</a></li>
             <li><a href="/privacyverklaring" target="_blank">privacy</a> |
@@ -10,8 +10,8 @@
             <li><a href="/internationalisering">internationalisering</a></li>
         </ul>
     </div>
-    <div class="menu w-1/6 pb-5 pl-10 relative md:w-1/4 sm:w-1/2 xxs:w-full">
-        <h3 class="font-bold text-white text-base">cursisten</h3>
+    <div class="relative w-full pb-5 pl-10 lg:w-1/6 menu md:w-1/4 sm:w-1/2">
+        <h3 class="text-base font-bold text-white">cursisten</h3>
         <ul>
             <li><a href="https://moodle.cvovolt.be" target="_blank">leerplatform</a></li>
             <li><a href="/lokaal">lokaal</a></li>
@@ -19,16 +19,16 @@
         </ul>
     </div>
     <div class="break narrow"></div>
-    <div class="menu w-1/6 pb-5 pl-10 relative md:w-1/4 sm:w-1/2 xxs:w-full">
-        <h3 class="font-bold text-white text-base">informatie</h3>
+    <div class="relative w-full pb-5 pl-10 menu lg:w-1/6 md:w-1/4 sm:w-1/2">
+        <h3 class="text-base font-bold text-white">informatie</h3>
         <ul>
             <li><a href="/contact">contact</a></li>
             <li><a href="/agenda">agenda</a></li>
             <li><a href="/infomomenten">infomomenten</a></li>
         </ul>
     </div>
-    <div class="menu w-1/6 pb-5 pl-10 relative md:w-1/4 sm:w-1/2 xxs:w-full">
-        <h3 class="font-bold text-white text-base">personeel</h3>
+    <div class="relative w-full pb-5 pl-10 menu lg:w-1/6 md:w-1/4 sm:w-1/2">
+        <h3 class="text-base font-bold text-white">personeel</h3>
         <ul>
             <li><a href="https://personeel.cvovolt.be" target="_blank">personeel.cvovolt.be</a></li>
             <li><a href="/vacatures">vacatures</a></li>
@@ -43,7 +43,7 @@
         cvo volt is een instelling van <a href="https://huis11.be/" target="_blank" class="copyrightInfo">Scholengroep
             Huis 11</a> - Ondernemingsnummer 0267.378.817<br>
     </p>
-    <div class="logos ml-auto min-w-24">
+    <div class="ml-auto logos min-w-24">
         <ul>
             <li class="sg11"><a href="https://huis11.be/" target="_blank">Scholengroep 11</a></li>
             <li class="go"><a href="https://www.g-o.be" target="_blank">GO!</a></li>
